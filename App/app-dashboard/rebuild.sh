3356d9c0e206ef8c227051318e0bb448f6e82d8f
Marco Griep <GriepMarco@gmail.com>
2020-06-18 17:38:05 +0200

    Update rebuild.sh

--- App/app-dashboard/rebuild.sh
+++ App/app-dashboard/rebuild.sh
@@ -1,5 +1,7 @@
 docker stop app-backup-rsync-apache-001
 docker rm app-backup-rsync-apache-001
+docker build -t app-backup-rsync-apache .
+npm install
 npm run build
 docker build -t app-backup-rsync-apache-001 .
-docker run -dit --name app-backup-rsync-apache-001 -p 80:80 app-backup-rsync-apache
+docker run -dit --name app-backup-rsync-apache-001 -p 15100:80 app-backup-rsync-apache
